633e2fa48fbd2b1a40b3050d8e5ef957e60169b9
whiteisnick <whiteisnick@gmail.com>
2015-05-14 22:30:29 -0400

    resized sample images

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -7,27 +7,27 @@ $(document).ready(function(){
 				currentSample = currentSample - 1;
 				$('.samples-photo').toggle();
 				$('.samples-photo').removeClass('col-xs-8 col-xs-offset-2').addClass('col-xs-12 col-xs-offset-0');
-				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.JPG" alt="Responsive image">');
 				$('.samples-photo').fadeToggle(700);
 				return(currentSample);
 			} else if(currentSample === 4) {
 				currentSample = currentSample - 1;
 				$('.samples-photo').toggle();
 				$('.samples-photo').removeClass('col-xs-12 col-xs-offset-0').addClass('col-xs-8 col-xs-offset-2');
-				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.JPG" alt="Responsive image">');
 				$('.samples-photo').fadeToggle(700);
 				return(currentSample);
 			} else {
 				currentSample = currentSample -1;
 				$('.samples-photo').toggle();
-				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.JPG" alt="Responsive image">');
 				$('.samples-photo').fadeToggle(700);
 			};
 		} else {
 			currentSample = 7;
 			$('.samples-photo').toggle();
 			$('.samples-photo').removeClass('col-xs-12 col-xs-offset-0').addClass('col-xs-8 col-xs-offset-2');
-			$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+			$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.JPG" alt="Responsive image">');
 			$('.samples-photo').fadeToggle(700);
 			return(currentSample);
 		};
@@ -38,27 +38,27 @@ $(document).ready(function(){
 				currentSample = currentSample + 1;
 				$('.samples-photo').toggle();
 				$('.samples-photo').removeClass('col-xs-12 col-xs-offset-0').addClass('col-xs-8 col-xs-offset-2');
-				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.JPG" alt="Responsive image">');
 				$('.samples-photo').fadeToggle(700);
 				return(currentSample);
 			} else if(currentSample == 3) {
 				currentSample = currentSample + 1;
 				$('.samples-photo').toggle();
 				$('.samples-photo').removeClass('col-xs-8 col-xs-offset-2').addClass('col-xs-12 col-xs-offset-0');
-				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.JPG" alt="Responsive image">');
 				$('.samples-photo').fadeToggle(700);
 				return(currentSample);
 			} else {
 				currentSample = currentSample + 1;
 				$('.samples-photo').toggle();
-				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+				$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.JPG" alt="Responsive image">');
 				$('.samples-photo').fadeToggle(700);
 			}
 		} else {
 			currentSample = 1;
 			$('.samples-photo').toggle();
 			$('.samples-photo').removeClass('col-xs-8 col-xs-offset-2').addClass('col-xs-12 col-xs-offset-0');
-			$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.jpg" alt="Responsive image">');
+			$('#sample').html('<img class="img-responsive img-rounded" src="images/sample' + currentSample + '.JPG" alt="Responsive image">');
 			$('.samples-photo').fadeToggle(700);
 			return(currentSample);
 		};
